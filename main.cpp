50085892d059dbec316c67388a54748bff0ec8e5
chihara-shigou <k024g0039@g.neec.ac.jp>
2025-04-25 16:41:53 +0900

    修正完了

--- main.cpp
+++ main.cpp
@@ -29,9 +29,9 @@ Matrix4x4 MakeRotateYMatrix(float radian) {
     Matrix4x4 result = {};
 
     result.m[0][0] = std::cos(radian);
-    result.m[0][2] = std::sin(radian);
+    result.m[0][2] = -std::sin(radian);
     result.m[1][1] = 1.0f;
-    result.m[2][0] = -std::sin(radian);
+    result.m[2][0] = std::sin(radian);
     result.m[2][2] = std::cos(radian);
     result.m[3][3] = 1.0f;
 
@@ -43,8 +43,8 @@ Matrix4x4 MakeRotateZMatrix(float radian) {
     Matrix4x4 result = {};
 
     result.m[0][0] = std::cos(radian);
-    result.m[0][1] = -std::sin(radian);
-    result.m[1][0] = std::sin(radian);
+    result.m[0][1] = std::sin(radian);
+    result.m[1][0] = -std::sin(radian);
     result.m[1][1] = std::cos(radian);
     result.m[2][2] = 1.0f;
     result.m[3][3] = 1.0f;
